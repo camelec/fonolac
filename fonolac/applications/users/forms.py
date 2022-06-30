b3a8520a64d590bf33908fd1bb86a826751f4f25
camelec <camilo.sepmar@gmail.com>
2022-06-29 23:16:23 -0400

    Configuración de registro de usuarios, falta verificar contraseña

---
+++ fonolac/applications/users/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import User
+
+class UserRegisterForm(forms.ModelForm):
+    """Form definition for UserRegister."""
+
+    password1 = forms.CharField(
+        label = 'Contraseña',
+        required = True,
+        widget = forms.PasswordInput(
+            attrs = {
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label = 'Contraseña',
+        required = True,
+        widget = forms.PasswordInput(
+            attrs = {
+                'placeholder': 'Repetir Contraseña '
+            }
+        )
+    )
+
+    class Meta:
+        """Meta definition for UserRegisterform."""
+
+        model = User
+        fields = (
+            'username',
+            'email',
+            'nombres',
+            'apellidos',
+            'genero',
+        )
