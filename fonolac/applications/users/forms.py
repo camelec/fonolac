58ce4f94fb974d2747830f878e82c5d52e290722
camelec <camilo.sepmar@gmail.com>
2022-07-01 00:10:11 -0400

    Creación Register, Login, Update. Pero falta revisar el Mixin, al intentar entrar a la url update/ debieses redirigir a login/ y no lo hace

--- fonolac/applications/users/forms.py
+++ fonolac/applications/users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import User
+from django.contrib.auth import authenticate
 
 class UserRegisterForm(forms.ModelForm):
     """Form definition for UserRegister."""
@@ -34,3 +35,57 @@ class UserRegisterForm(forms.ModelForm):
             'apellidos',
             'genero',
         )
+    
+    def clean_password2(self):
+        if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+            self.add_error('password2', 'La contraseña no coincide')
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label = 'username',
+        required = True,
+        widget = forms.TextInput(
+            attrs = {
+                'placeholder': 'username'
+            }
+        )
+    )
+    password = forms.CharField(
+        label = 'Contraseña',
+        required = True,
+        widget = forms.PasswordInput(
+            attrs = {
+                'placeholder': 'Contraseña'
+            }
+        )
+    )
+
+    def clean(self):
+        cleaned_data = super(LoginForm, self).clean()
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+
+        if not authenticate(username = username, password = password):
+            raise forms.ValidationError('Los datos de ususrio no son correctos')
+        
+        return self.cleaned_data
+
+class UpdatePasswordForm(forms.Form):
+    password1 = forms.CharField(
+        label = 'Contraseña',
+        required = True,
+        widget = forms.PasswordInput(
+            attrs = {
+                'placeholder': 'Contraseña actual'
+            }
+        )
+    )
+    password2 = forms.CharField(
+        label = 'Contraseña',
+        required = True,
+        widget = forms.PasswordInput(
+            attrs = {
+                'placeholder': 'Contraseña nueva'
+            }
+        )
+    )
--- fonolac/applications/users/views.py
+++ fonolac/applications/users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.views.generic import (
+    View,
     CreateView
 )
 from django.views.generic.edit import (
@@ -8,7 +13,11 @@ from django.views.generic.edit import (
 
 # Create your views here.
 
-from .forms import UserRegisterForm
+from .forms import (
+    UserRegisterForm, 
+    LoginForm,
+    UpdatePasswordForm
+)
 from .models import User
 
 class UserRegisterView(FormView):
@@ -28,3 +37,46 @@ class UserRegisterView(FormView):
         )
         #
         return super(UserRegisterView, self).form_valid(form)
+
+class LoginUser(FormView):
+    template_name = 'users/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('home_app:panel')
+    
+    def form_valid(self, form):
+        user = authenticate(
+            username = form.cleaned_data['username'],
+            password  = form.cleaned_data['password']
+
+        )
+        login(self.request, user)
+        return super(LoginUser, self).form_valid(form)
+
+class LogoutView(View):
+
+    def get(self, request, *args, **kargs):
+        logout(request)
+        return HttpResponseRedirect(
+            reverse(
+                'users_app:user-login'
+            )
+        )
+
+class UpdatePasswordView(LoginRequiredMixin, FormView):
+    template_name = 'users/update.html'
+    form_class = UpdatePasswordForm
+    success_url = reverse_lazy('users_app:user-login')
+    login_url = reverse_lazy('users_app:user-login') # Propiedad para el LoginRequiredMixin
+    
+    def form_valid(self, form):
+        usuario = self.request.user
+        user = authenticate(
+            username = usuario.username,
+            password = form.cleaned_data['password1']
+        )
+        if user:
+            new_password = form.cleaned_data['password2']
+            usuario.set_password(new_password)
+            usuario.save()
+        logout(self.request)
+        return super(UpdatePasswordView, self).form_valid(form)
